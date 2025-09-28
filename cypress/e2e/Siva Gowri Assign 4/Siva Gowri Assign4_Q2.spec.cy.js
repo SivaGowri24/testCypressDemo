d614a120895e0af35a934fec722c0d83dba0b697
Chitrada <gowri.chitrada@gmail.com>
2025-09-28 16:28:23 +0530

    Adding updated code to the repository

--- cypress/e2e/Siva Gowri Assign 4/Siva Gowri Assign4_Q2.spec.cy.js
+++ cypress/e2e/Siva Gowri Assign 4/Siva Gowri Assign4_Q2.spec.cy.js
@@ -5,25 +5,25 @@ describe('Registration Form Validation', () => {
      });
 
   it.only('Registration Successful', () => {
-    cy.get('input[name="username"]').type('SivaGowri@12345');
-    cy.get('#password').type('SGowri@1234567!!');
-    cy.get('#confirmPassword').type('SGowri@1234567!!');
+    cy.get('input[name="username"]').type('SivaGowrii12345');
+    cy.get('#password').type('SGowrii1234567!!');
+    cy.get('#confirmPassword').type('SGowrii1234567!!');
     cy.get('button[type="submit"]').should('be.visible').and('contain.text', 'Register').click();
     cy.get('.alert-info').should('be.visible').and('contain', 'Successfully registered, you can log in now.');
     })
 
    it('Password Mismatch', () => {
-    cy.get('input[name="username"]').type('SivaGowri@12345');
-    cy.get('#password').type('PasworrdMismatch@!!');
-    cy.get('#confirmPassword').type('SGowri@1234567!!');
+    cy.get('input[name="username"]').type('SivaGowrii12345');
+    cy.get('#password').type('PasworrdMismatchh!!');
+    cy.get('#confirmPassword').type('SGowrii1234567!!');
     cy.get('button[type="submit"]').should('be.visible').and('contain.text', 'Register').click();
     // Wait up to 10 seconds for the success message
     cy.get('.alert-info', { timeout: 10000 }).should('be.visible').and('contain', 'Successfully registered, you can log in now.');
 })
 
 it('Empty Field Error Verification', () => {
-    cy.get('input[name="username"]').type('SivaGowri@12345');
-    cy.get('#confirmPassword').type('SGowri@1234567!!');
+    cy.get('input[name="username"]').type('SivaGowrii12345');
+    cy.get('#confirmPassword').type('SGowrii1234567!!');
     cy.get('button[type="submit"]').should('be.visible').and('contain.text', 'Register').click();
     cy.get('#flash').should('be.visible').and('contain.text', 'All fields are required.');
  
